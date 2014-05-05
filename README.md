21017e0f5945169e2172544f0434e80927a50d85
Randy Hicks <randy@visualcoma.com>
2014-05-05 07:24:41 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-LifeBoatCoaching
-================
+ChildTheme
+====================
 
-child theme of ParentTheme for LifeBoatCoaching.org
+This child theme is the most basic addition to it's parent theme. There are numerous classes nested with in ParentTheme that utilize wrapper functions for rapid development.
---
+++ addons/initiate-addons.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File Name initiate-addons.php
+ * @package WordPress
+ * @subpackage ParentTheme
+ * @license GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @version 1.2
+ * @updated 10.02.13
+ *
+ * Description:
+ * Include core functionality, activation and theme functions.
+ **/
+#################################################################################################### */
+
+
+if ( ! defined('THEME_ADDONS_INIT') ) {
+	
+	// Init ParentTheme_VC lib
+	require_once( get_template_directory() . "/includes/initiate-lib.php" );
+	
+	// Added Functionality
+	// require_once( "ThemeSupport/initiate.php" );
+	require_once( "AdminCustomizationsWP.php" );
+	require_once( "WPSEOEdits.php" );
+	
+	define( 'THEME_ADDONS_INIT', true );
+	
+} // end if ( ! defined('THEME_ADDONS_INIT') )
