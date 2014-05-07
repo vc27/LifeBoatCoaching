8da0f663d3864ea776160f11a1ec25af59cf8ca0
Randy Hicks <randy@visualcoma.com>
2014-05-07 08:21:11 -0500

    dev 0.3.0 - css cleanup and frame adjustments

--- change-log.md
+++ change-log.md
@@ -1,6 +1,9 @@
 ChangeLog
 ====================
 
+### 05.07.14 - dev 0.3.0
+- css cleanup and frame adjustments
+
 ### 05.06.14 - dev 0.2.0
 - round 1 basic frame
 
--- page.php
+++ page.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * File Name index.php
+ * File Name page.php
  * @package WordPress
  * @subpackage ParentTheme
  * @license GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * @version 1.9
+ * @version 1.7
  * @updated 01.20.14
  **/
 #################################################################################################### */
@@ -14,7 +14,7 @@ get_template_part( 'header' );
 ?>
 <div class="row-fluid">
 	<div class="span10 offset1">
-		<?php get_template_part( 'loop-default' ); ?>
+		<?php get_template_part( 'loop-page-default' ); ?>
 	</div>
 </div>
 <?php
